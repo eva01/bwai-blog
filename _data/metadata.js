4122fa6d6d8cc9c9c1f87e2749571d8e1f79b301
eva01 <4400814+eva01@users.noreply.github.com>
2025-10-20 20:07:51 +0800

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Build With AI",
+	url: "https://buildwithai.my/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Build with AI vibe coding community",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Shee Tze Jin",
+		email: "jin@wemaker.space",
+		url: "https://wemaker.space"
 	}
 }
